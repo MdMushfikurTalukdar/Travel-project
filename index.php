6793fd95d33609fe3727b74aeef1e92989b19622
Meem <jmeem191012@bscse.uiu.ac.bd>
2022-11-29 01:04:01 +0600

    Inserted code for homepage

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="./css/styleLogin.css">
+    <title>Travel</title>
+</head>
+<body>
+    <section class="hero">
+        <div class="container">
+            <div class="nav row pt-4">
+                <div class="col-6 text-light"> <h4>Travel & Explore</h4></div>
+                <div class="col-6 d-flex justify-content-end"> <a href="sign_in.php" class="btn btn-outline-light rounded-pill">Sign up Now</a></div>
+            </div>
+            </div>
+            <div class="header text-center pt-5 text-light">
+                <h2>It's a big world out there, <br> Go Explore</h2>
+                <p class="pt-4">Thinking of taking a break from every days Busy Life? Don't Worry, <br> we take care of your trip</p>
+            </div>
+            <div class="text-light text-center pt-5"><a href="loginuser.php" class="btn btn-outline-light rounded-pill">Login as User</a></div>
+        </div>
+    </section>
+    
+    
+
+
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+</body>
+</html>
